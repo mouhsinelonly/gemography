2bae84eba1d3a417f94d75feeca9979672dd8226
mouhsine bakhich <mouhsine.bakhich@gmail.com>
2021-03-24 16:49:42 +0400

    add react-testing-library and jest

---
+++ jest.setup.ts
@@ -0,0 +1,7 @@
+// jest.setup.ts
+import '@testing-library/jest-dom';
+// setup file
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
