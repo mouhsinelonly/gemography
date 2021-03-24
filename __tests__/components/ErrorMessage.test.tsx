efbcea88e4debf7de9b0204a5c921d2f0a1db2f4
mouhsine bakhich <mouhsine.bakhich@gmail.com>
2021-03-24 17:06:29 +0400

    write unit tests for ErrorMessage component

--- __tests__/components/ErrorMessage.test.tsx
+++ __tests__/components/ErrorMessage.test.tsx
@@ -2,11 +2,22 @@ import { render } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import { ErrorMessage } from "@components/ErrorMessage"
 
-describe("search form", () => {
-    test("has form element", () => {
-        const { getByTestId } = render(<ErrorMessage message='hello' />);
+const message = 'Error Message'
 
+describe("ErrorMessage component", () => {
+    it("renders", () => {
+        const { getByTestId } = render(<ErrorMessage message={message} />);
         expect(getByTestId("error-message")).toBeInTheDocument();
+    });
+
+    it("shows error message", () => {
+        const { getByText } = render(<ErrorMessage message={message} />);
+        expect(getByText(message)).toBeInTheDocument();
+    });
+
+    it("has red background", () => {
+        const { getByText } = render(<ErrorMessage message={message} />);
+        expect(getByText(message)).toHaveClass("bg-red-500");
     });
 
 })
